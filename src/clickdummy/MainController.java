ef0b1fdbaeaa11c6a6115c229dcfad0e4f3d5b42
enthye <37763349+enthye@users.noreply.github.com>
2018-09-16 11:33:52 +1200

    initial stuff

---
+++ src/clickdummy/MainController.java
@@ -0,0 +1,25 @@
+package clickdummy;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+
+    @FXML
+    private Button rentalsButton;
+
+    @FXML
+    private Button customersButton;
+
+    @FXML
+    void customersButtonClicked(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void rentalsButtonClicked(ActionEvent event) {
+    	
+    }
+
+}
