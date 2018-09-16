7069f669228a2b9ec8d1904bac184fd3a208a5fe
enthye <37763349+enthye@users.noreply.github.com>
2018-09-16 14:49:33 +1200

    rental entry

---
+++ src/clickdummy/RentalEntryController.java
@@ -0,0 +1,25 @@
+package clickdummy;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RentalEntryController {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void addButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelButtonPressed(ActionEvent event) {
+
+    }
+
+}
---
+++ src/clickdummy/RentalsController.java
@@ -0,0 +1,44 @@
+package clickdummy;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class RentalsController {
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    void addButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchButtonPressed(ActionEvent event) {
+
+    }
+
+}
