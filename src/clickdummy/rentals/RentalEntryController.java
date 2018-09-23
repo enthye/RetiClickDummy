1c546421da27e82a57de302764a7d79f3931165d
enthye <37763349+enthye@users.noreply.github.com>
2018-09-24 10:40:38 +1200

    update windows

--- src/clickdummy/rentals/RentalEntryController.java
+++ src/clickdummy/rentals/RentalEntryController.java
@@ -1,43 +1,37 @@
 package clickdummy.rentals;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class RentalEntryController {
 
-	@FXML
-	private Button addButton;
-
-	@FXML
-	private Button cancelButton;
-
-	@FXML
-	private TextField startDateField;
-
-	@FXML
-	private TextField endDateField;
-
-	@FXML
-	private TextField customerField;
-
-	@FXML
-	private TextField carField;
-
-	@FXML
-	private TextField rentalRateField;
+	@FXML private AnchorPane mainRoot;
+	@FXML private Button addButton,cancelButton;
+	@FXML private TextField startDateField,endDateField,
+	customerField,carField,rentalRateField;
 
 	@FXML
 	void addButtonPressed(ActionEvent event) {
-		
+
 	}
 
 	@FXML
 	void cancelButtonPressed(ActionEvent event) {
-		Stage stage = (Stage) cancelButton.getScene().getWindow();
-	    stage.close();
+		AnchorPane root = null;
+
+		try {
+			root = FXMLLoader.load(getClass().getResource("/resources/Rentals.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		mainRoot.getChildren().setAll(root);
 	}
 
 }
